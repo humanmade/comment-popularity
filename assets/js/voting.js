dd0e3b85b1ac47c3342b2a282cd5b7432481b1e6
Paul de Wouters <paul@hmn.md>
2014-07-20 13:42:11 +0200

    Use weight terminology instead of karma
    Signed-off-by: Paul de Wouters <paul@hmn.md>

--- assets/js/voting.js
+++ assets/js/voting.js
@@ -4,19 +4,19 @@
 	$(function () {
 
 		// catch the upvote/downvote action
-		$( 'div.karma' ).on( 'click', 'span > a', function( e ){
+		$( 'div.comment-weight-container' ).on( 'click', 'span > a', function( e ){
 			e.preventDefault();
 			var value = 0;
 			var comment_id = $(this).data('commentId');
-			if ( $(this).hasClass( 'add-karma' ) ) {
+			if ( $(this).hasClass( 'vote-up' ) ) {
 				value = 1;
-			} else if( $(this).hasClass( 'remove-karma' ) ) {
+			} else if( $(this).hasClass( 'vote-down' ) ) {
 				value = -1;
 			}
 
 			$.post( comment_popularity.ajaxurl, {action: 'comment_vote', vote: value, comment_id: comment_id, hmn_vote_nonce: comment_popularity.hmn_vote_nonce}, function( data ){
 				// update karma
-				$( 'div#comment-' + data.data.comment_id + ' span.comment-karma' ).text( data.data.karma );
+				$( 'div#comment-' + data.data.comment_id + ' span.comment-weight' ).text( data.data.weight );
 			});
 		});
 
