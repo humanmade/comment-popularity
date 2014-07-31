076f818067d684dc404b280d5a01379b8588ea51
Paul de Wouters <paul@hmn.md>
2014-07-31 13:55:07 +0200

    New plugin test class

---
+++ tests/test-plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+class PluginTest extends WP_UnitTestCase {
+
+	function testCommentAuthorKarmaIsAnInt() {
+		// replace this with some actual testing code
+		$this->assertInte( true );
+	}
+}
+
