9a2407e8c694467d40d6a73851cb9c98ac8a9b22
Paul de Wouters <paul@hmn.md>
2014-08-05 12:19:03 -0300

    update tests config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,22 @@
 <?php
-
 /**
  * Bootstrap the plugin unit testing environment.
  *
- * Edit 'active_plugins' setting below to point to your main plugin file.
- *
- * @package wordpress-plugin-tests
+ * @package WordPress
+ * @subpackage JSON API
  */
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'comment-popularity/comment-popularity.php' ),
+	'active_plugins' => array( basename( dirname( dirname( __FILE__ ) ) ) . '/comment-popularity.php' ),
 );
 
 // If the develop repo location is defined (as WP_DEVELOP_DIR), use that
-// location. Otherwise, we'll just assume that it's a Travis CI build
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress develop SVN checkout.
 
 if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
 } else {
-	require '/tmp/wordpress-tests-lib/tests/phpunit/includes/bootstrap.php';
+	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
