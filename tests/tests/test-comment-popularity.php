b99d3757c897e78d6cf072c19dacb6a86233752e
Paul de Wouters <paul@hmn.md>
2014-08-05 09:25:01 -0300

    formatting and cleanup
    Signed-off-by: Paul de Wouters <paul@hmn.md>

--- tests/tests/test-comment-popularity.php
+++ tests/tests/test-comment-popularity.php
@@ -2,21 +2,15 @@
 
 Class Test_HMN_Comment_Popularity extends HMN_Comment_PopularityTestCase {
 
-	const CLASS_NAME = 'HMN_Comment_Popularity';
-
 	protected $test_user_id;
 
 	protected $test_post_id;
 
 	protected $test_comment_id;
 
-	protected $instance;
-
 	public function setUp() {
 
 		parent::setUp();
-
-		$this->instance = HMN_Comment_Popularity::get_instance();
 
 		$this->test_user_id = $this->factory->user->create(
 			array(
@@ -50,33 +44,46 @@ Class Test_HMN_Comment_Popularity extends HMN_Comment_PopularityTestCase {
 
 		parent::tearDown();
 
-		$this->instance = null;
+		$this->plugin = null;
 
 		wp_delete_comment( $this->test_comment_id );
 
 		wp_delete_post( $this->test_post_id );
 
+		delete_user_meta( $this->test_user_id, 'comments_voted_on' );
+
 		wp_delete_user( $this->test_user_id );
 	}
 
-	/**
-	 * Check if get instance function return a valid instance of the strem class
-	 *
-	 * @return void
-	 */
-	public function test_get_instance() {
+	protected function add_cap() {
 
-		$this->instance = HMN_Comment_Popularity::get_instance();
+		$role = get_role( 'administrator' );
 
-		$this->assertInstanceOf( 'HMN_Comment_Popularity', $this->instance );
+		if ( ! $role->has_cap( 'vote_on_comments' ) ) {
+
+			$role->add_cap( 'vote_on_comments' );
+
+		}
+	}
+
+	protected function remove_cap() {
+
+		$role = get_role( 'administrator' );
+
+		if ( ! empty( $role ) ) {
+
+			$role->remove_cap( 'vote_on_comments' );
+
+		}
 
 	}
 
+
 	public function test_too_soon_to_vote_again() {
 
-		$this->instance->comment_vote( 1, $this->test_comment_id );
+		$this->plugin->comment_vote( 1, $this->test_comment_id );
 
-		$ret = $this->instance->comment_vote( -1, $this->test_comment_id );
+		$ret = $this->plugin->comment_vote( -1, $this->test_comment_id );
 
 		$this->assertEquals( 'voting_flood', $ret['error_code'] );
 
@@ -84,17 +91,26 @@ Class Test_HMN_Comment_Popularity extends HMN_Comment_PopularityTestCase {
 
 	public function test_has_permission_to_vote() {
 
+		$this->remove_cap();
+
+		$ret = $this->plugin->comment_vote( 1, $this->test_comment_id );var_dump($ret);
+
+		$this->assertEquals( 'insufficient_permissions', $ret['error_code'] );
+
+		$this->add_cap();
+
 	}
 
 	public function test_prevent_same_vote_twice() {
 
-		$this->instance->comment_vote( 1, $this->test_comment_id );
+		$this->plugin->comment_vote( 1, $this->test_comment_id );
 
-		$ret = $this->instance->comment_vote( 1, $this->test_comment_id );
+		$ret = $this->plugin->comment_vote( 1, $this->test_comment_id );
 
 		sleep( 7 );
 
 		$this->assertEquals( 'same_action', $ret['error_code'] );
+
 	}
 
 	public function test_upvote_comment() {
@@ -103,11 +119,11 @@ Class Test_HMN_Comment_Popularity extends HMN_Comment_PopularityTestCase {
 
 		$action = 'upvote';
 
-		$result = $this->instance->update_comments_voted_on_for_user( $this->test_user_id, $this->test_comment_id, $action );
+		$result = $this->plugin->update_comments_voted_on_for_user( $this->test_user_id, $this->test_comment_id, $action );
 
 		$expected = array(
-				'vote_time' => $vote_time,
-				'last_action' => $action
+			'vote_time' => $vote_time,
+			'last_action' => $action
 		);
 
 		$this->assertEquals( $expected, $result );
