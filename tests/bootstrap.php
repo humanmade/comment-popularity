106a20f08b1b7cf46cc379b92c1c0117cff8bde5
Paul de Wouters <paul@hmn.md>
2014-08-05 11:02:05 -0300

    Make tests work locally

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,23 @@
 <?php
+/**
+ * Bootstrap the plugin unit testing environment.
+ *
+ * Edit 'active_plugins' setting below to point to your main plugin file.
+ *
+ * @package wordpress-plugin-tests
+ */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/srv/www/wordpress-develop.dev/tests';
+// Activates this plugin in WordPress so it can be tested.
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( 'comment-popularity/comment-popularity.php' ),
+);
 
-require_once $_tests_dir . '/phpunit/includes/functions.php';
+// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress develop SVN checkout.
 
-function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../comment-popularity.php';
+if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
+} else {
+	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-require $_tests_dir . '/phpunit/includes/bootstrap.php';
-require dirname( __FILE__ ) . '/testcase.php';
-
