2724ac6153f9e39d32015fbe97736879f80fbb82
Paul de Wouters <paul@hmn.md>
2014-08-13 17:23:41 -0300

    Bump to version 1.1.1

--- inc/class-comment-popularity.php
+++ inc/class-comment-popularity.php
@@ -10,7 +10,7 @@ class HMN_Comment_Popularity {
 	/**
 	 * Plugin version number.
 	 */
-	const HMN_CP_PLUGIN_VERSION = '1.1.0';
+	const HMN_CP_PLUGIN_VERSION = '1.1.1';
 
 	/**
 	 * The minimum PHP version compatibility.
