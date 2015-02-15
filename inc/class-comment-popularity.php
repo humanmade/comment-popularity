23a9fa774a8a29e2ac1aafe1a6e18d1df6cd7892
paul de wouters <paul@hmn.md>
2015-02-15 10:09:38 +0000

    Update class plugin version

--- inc/class-comment-popularity.php
+++ inc/class-comment-popularity.php
@@ -9,7 +9,7 @@ class HMN_Comment_Popularity {
 	/**
 	 * Plugin version number.
 	 */
-	const HMN_CP_PLUGIN_VERSION = '1.3.3';
+	const HMN_CP_PLUGIN_VERSION = '1.3.4';
 
 	/**
 	 * The minimum PHP version compatibility.
