6534d0edfb8efde42b89e0c06e823fce1bb29110
pdewouters <paul@hmn.md>
2017-09-13 09:51:18 +0100

    Fix upgrade routine

--- inc/upgrade.php
+++ inc/upgrade.php
@@ -9,38 +9,38 @@ defined( 'ABSPATH' ) || exit;
  */
 function hmn_cp_trigger_upgrades() {
 
-	// Get actual plugin version.
-	$current_version = HMN_Comment_Popularity::HMN_CP_PLUGIN_VERSION;
-
 	// Get latest version stored in DB option.
 	$hmn_cp_plugin_version = get_option( 'hmn_cp_plugin_version' );
 
-	// If the option doesn't exist, then we are upgrading from a version older than 1.2.1.
-	if ( ! $hmn_cp_plugin_version ) {
-		$hmn_cp_plugin_version = '1.2.0';
-
-		// Start tracking versions.
-		add_option( 'hmn_cp_plugin_version', $hmn_cp_plugin_version );
+	// No need to do anything if current version.
+	if ( version_compare( $hmn_cp_plugin_version, HMN_Comment_Popularity::HMN_CP_PLUGIN_VERSION, '>=' ) ) {
+		return;
 	}
 
-	// Determine if we need to run upgrade routine.
-	if ( version_compare( $hmn_cp_plugin_version, $current_version, '<' ) ) {
-		hmn_cp_v121_upgrade();
-
-		// Bump the version number in the DB.
+	// If DB option not present, treat as new install.
+	if ( ! $hmn_cp_plugin_version ) {
 		update_option( 'hmn_cp_plugin_version', HMN_Comment_Popularity::HMN_CP_PLUGIN_VERSION );
 	}
 
+	// Do we have users with the old option key? Upgrade in batches of 100.
+	$user_query = new WP_User_Query( array(
+		'number'   => 100,
+		'meta_key' => 'comments_voted_on',
+	) );
+	$users = $user_query->get_results();
+	if ( ! empty( $users ) ) {
+		hmn_cp_v121_upgrade( $users );
+	}
 }
 
 add_action( 'admin_init', 'hmn_cp_trigger_upgrades' );
 
 /**
  * Copy unprefixed option value to new prefixed option.
+ *
+ * @param array $users Users who need to be upgraded.
  */
-function hmn_cp_v121_upgrade() {
-
-	$users = get_users();
+function hmn_cp_v121_upgrade( array $users ) {
 
 	foreach ( $users as $user ) {
 
